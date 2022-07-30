2a3d70ba668651cff1d8f009149fbb14cbf57537
Barogthor <f.thery3103@gmail.com>
2022-07-30 12:56:15 +0200

    try list view

--- gui/src/lib.rs
+++ gui/src/lib.rs
@@ -1,6 +1,6 @@
 pub use eframe;
 use eframe::egui;
-use eframe::egui::ScrollArea;
+use eframe::egui::{ScrollArea, Ui};
 use eframe::egui::TextStyle;
 
 struct NewPortfolioState {
@@ -46,29 +46,30 @@ impl eframe::epi::App for AppGUI {
             ui.horizontal(|ui| {
                 ui.label("Nom portefeuille: ");
                 ui.text_edit_singleline(&mut self.state.new_porfolio.name);
+                if ui.button("Créer portefeuille").clicked() && self.state.new_porfolio.name.len() > 0 {
+                    let new_portfolio = std::mem::replace(&mut self.state.new_porfolio.name, String::new());
+                    self.state.portfolios.push(new_portfolio);
+                    println!("portfolios: {:?}", self.state.portfolios);
+                }
             });
-            if ui.button("Créer portefeuille").clicked() && self.state.new_porfolio.name.len() > 0 {
-                let new_portfolio = std::mem::replace(&mut self.state.new_porfolio.name, String::new());
-                self.state.portfolios.push(new_portfolio);
-                println!("portfolios: {:?}", self.state.portfolios);
-            }
-
+            ui.separator();
             let text_style = TextStyle::Body;
             let row_height = ui.text_style_height(&text_style);
             ScrollArea::vertical().auto_shrink([false;2])
                 .show_rows(ui,
-                    row_height,
-                   self.state.portfolios.len(),
-                   |ui, row_range| {
-                           for row in row_range {
-                               ui.text_edit_singleline(&mut self.state.portfolios[row]);
+                           row_height,
+                           self.state.portfolios.len(),
+                           |ui, row_range| {
+                               for row in row_range {
+                                   ui.label(&self.state.portfolios[row]);
+                               }
                            }
-                       }
                 );
+
         });
     }
 
     fn name(&self) -> &str {
-        "TKL Boursery"
+        "Boursery advisor"
     }
 }
